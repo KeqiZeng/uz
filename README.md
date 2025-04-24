63fd4557100eafc50dc4c998d99e0b3ce8a618d1
Keqi Zeng <ketchz0206@outlook.com>
2025-04-24 15:22:40 +0200

    chore: simplified

--- README.md
+++ README.md
@@ -19,37 +19,37 @@
 - `zsh`
 - `git`
 
-## Installation
-
-Just clone from GitHub
-
-```sh
-git clone https://github.com/KeqiZeng/uz.git ~/.uz
-```
-
 ## Usage
+
+### Installation ###
+
+Add the following lines to your `.zshrc` file, feel free to change the `UZ_PATH` as you like:
+```zsh
+export UZ_PATH=$HOME/.uz
+[[ -d $UZ_PATH ]] || git clone https://github.com/KeqiZeng/uz $UZ_PATH
+```
 
 ### Add Plugins
 
 ```zsh
 # in .zshrc
-declare -A plugins
+typeset -a plugins
 
 plugins=(
-  ['zsh-users/zsh-completions']="( [completion]=0 [frozen]=1 )" # 0: true 1: false
-  ['esc/conda-zsh-completion']="( [completion]=0 [frozen]=1 )"
-  ['Aloxaf/fzf-tab']="( [completion]=1 [frozen]=1 )"
-  ['zsh-users/zsh-autosuggestions']="( [completion]=1 [frozen]=1 )"
-  ['hlissner/zsh-autopair']="( [completion]=1 [frozen]=0 )"
-  ['zdharma-continuum/fast-syntax-highlighting']="( [completion]=1 [frozen]=1 )"
+  zsh-users/zsh-completions
+  esc/conda-zsh-completion
+  Aloxaf/fzf-tab
+  zsh-users/zsh-autosuggestions
+  hlissner/zsh-autopair
+  zsh-users/zsh-syntax-highlighting
 )
 
-source ~/.dotfiles/zsh/uz/uz.zsh
+source ${UZ_PATH}/uz.zsh
 ```
-**Note:** If a plugin is a completion plugin, please set `completion` to `0`. If you don't want `µz` to update a plugin, set `frozen` to `0`.
-Then `source ~/.zshrc` or reopen the Terminal Emulator and run `zinstall`. `µz` will clone plugins in parallel.
 
-You don't need to do `compinit`, `µz` will do it for you. All the completion plugins will be loaded before `compinit`, others will be loaded after. The `zcompdump` file will be stored in the directory `~/.cache/zsh`.
+Then `source ~/.zshrc` or reopen the Terminal Emulator and run `zinstall`. `µz` will clone plugins from GitHub in parallel.
+
+You don't need to do `compinit`, `µz` will do it for you. All the completion plugins will be loaded before `compinit`. The `zcompdump` file will be stored in the directory `~/.cache/zsh`.
 
 By default `µz` will source `init.zsh` or `plugin_name.(zsh|plugin.zsh|zsh-theme|sh)` automatically, but you can also tell specify another script to the `zload` command as follows:
 
@@ -72,8 +72,4 @@ export UZ_PLUGIN_PATH=${UZ_PATH}/plugins # default
 
 ## Uninstall
 
-~~`μz` only creates folders for the cloned modules and, by default, are self contained into the installation directory.~~
-
-For pretty output, `μz` will create `.uz_cache` folder in `/tmp` directory, when run `zinstall` and `zupdate`. But don't worry about it, once the commands are over, `.uz_cache` will be removed.
-
-To uninstall remove the installation directory (`$UZ_PATH`) and the modules folder (`$UZ_PLUGIN_PATH`) if applicable.
+To uninstall simply remove the installation directory (`$UZ_PATH`) and the modules folder (`$UZ_PLUGIN_PATH`) if applicable.
--- uz.zsh
+++ uz.zsh
@@ -1,144 +1,125 @@
+#!/usr/bin/env zsh
+
+rm -rf /tmp/.uz_cache
 unset UZ_PLUGINS
-typeset UZ_PATH=${0:A:h}
+# typeset UZ_PATH=${0:A:h}
 typeset UZ_PLUGIN_PATH=${UZ_PLUGIN_PATH:-${UZ_PATH}/plugins}
 typeset -a UZ_PLUGINS
 
 zinstall() {
 {
-  zadd() {
+    zadd() {
 		local zmodule=${1:t} zurl=${1}
 		local zpath=${UZ_PLUGIN_PATH}/${zmodule}
 		mkdir -p ${zpath}
 		git clone --recursive https://github.com/${zurl}.git ${zpath}
-  }
+    }
 
-  local index1=1
-	local index2=1
-  declare -a installed_plugins to_install
-  mktemp -d /tmp/.uz_cache > /dev/null
-  # mkdir /tmp/.uz_cache
-  local cache_file=$(mktemp /tmp/.uz_cache/uz_cache.XXXX)
-  for plugin in ${(k)plugins[@]}; do
-		echo $plugin >> $cache_file
-  done
+    local index=1
+    local -aU installed_plugins to_install
+    mktemp -d /tmp/.uz_cache > /dev/null
+    local cache_file=$(mktemp /tmp/.uz_cache/uz_cache.XXXX)
+    for plugin in ${(k)plugins[@]}; do
+        echo $plugin >> $cache_file
+    done
 
-  # installed_plugins: the plugins in the UZ_PLUGIN_PATH and in the array plugins
-  if [ -d "$UZ_PLUGIN_PATH" ]; then
-		for dir in $(command ls -d ${UZ_PLUGIN_PATH}/* | awk -F'/' '{print $NF}'); do
-			installed_plugins[$index1]=$(sed -n /$dir/p $cache_file)
-			((index1++))
-		done
-  fi
+    # installed_plugins: the plugins in the UZ_PLUGIN_PATH and in the array plugins
+    if [[ -d "$UZ_PLUGIN_PATH" ]]; then
+        for full_path in "${UZ_PLUGIN_PATH}"/*(/N); do
+            local dir_name=${full_path:t}
+            installed_plugins[$index]=$(sed -n /$dir_name/p $cache_file)
+            ((index++))
+        done
+    fi
 
-  # echo ${installed_plugins[@]}
+    # echo ${installed_plugins[@]}
 
-  for p in ${(k)plugins[@]}; do
-		(( $installed_plugins[(I)$p] )) || { to_install[$index2]=$p; ((index2++)) }
-  done
-  # echo ${to_install[@]}
+    index=1
+    for p in ${(k)plugins[@]}; do
+		(( $installed_plugins[(I)$p] )) || { to_install[$index]=$p; ((index++)) }
+    done
+    # echo ${to_install[@]}
 
-  if [[ ${#to_install[@]} -eq 0 ]]; then
-		rm -rf /tmp/.uz_cache
-		return 1
-  fi
+    if [[ ${#to_install[@]} -eq 0 ]]; then
+        echo -e "\e[1;32mAll plugins are installed.\e[0m"
+	    rm -rf /tmp/.uz_cache
+        return 0
+    fi
 
-  local i=1
-  local j=1
-  echo "\e[1;32mInstalling: \e[0m"
-  for p in $to_install[@]; do
-		echo $p
-		file[$i]=$(mktemp /tmp/.uz_cache/uz_cache.XXXX)
-		zadd $p &> $file[$i] &
-		((i++))
-	done
+    local i=1
+    echo "\e[1;32mInstalling: \e[0m"
+    for p in $to_install[@]; do
+	    echo $p
+	    file[$i]=$(mktemp /tmp/.uz_cache/uz_cache.XXXX)
+	    zadd $p &> $file[$i] &
+	    ((i++))
+    done
 
-  wait
-  echo ""
+    wait
+    echo ""
 
-  for p in $to_install[@]; do
-    echo -ne "\e[1;32m$p: \e[0m\n"
-		command cat $file[$j]
-	((j++))
-  done
-  rm -rf /tmp/.uz_cache
-  echo "\ndone"
+    i=1
+    for p in $to_install[@]; do
+        echo -ne "\e[1;32m$p: \e[0m\n"
+		command cat $file[$i]
+	    ((i++))
+    done
+    rm -rf /tmp/.uz_cache
+    echo "\ndone"
 } | grep -Ev "^[\d{0,3}]$" }
 
 zload() {
-  local zmodule=${1:t} zurl=${1} zscript=${2}
-  local zpath=${UZ_PLUGIN_PATH}/${zmodule}
-  (( $UZ_PLUGINS[(I)$zpath] )) || UZ_PLUGINS+=("${zpath}")
+    local zmodule=${1:t} zurl=${1} zscript=${2}
+    local zpath=${UZ_PLUGIN_PATH}/${zmodule}
+    (( $UZ_PLUGINS[(I)$zpath] )) || UZ_PLUGINS+=("${zpath}")
 
-  local zscripts=(${zpath}/(init.zsh|${zmodule:t}.(zsh|plugin.zsh|zsh-theme|sh))(NOL[1]))
-  if    [[ -f ${zpath}/${zscript} ]]; then source ${zpath}/${zscript}
-  elif  [[ -f ${zscripts} ]]; then source ${zscripts}
-  else  echo -e "\e[1;31mNo scripts was found for:\e[0m \e[3m${zurl}\e[0m. \e[1;32mTry to run zinstall\e[0m"
-  fi
+    local zscripts=(${zpath}/(init.zsh|${zmodule:t}.(zsh|plugin.zsh|zsh-theme|sh))(NOL[1]))
+    if [[ -f ${zpath}/${zscript} ]]; then
+        source ${zpath}/${zscript}
+    elif [[ -f ${zscripts} ]]; then
+        source ${zscripts}
+    else
+        echo -e "\e[1;31mNo scripts was found for:\e[0m \e[3m${zurl}\e[0m. \e[1;32mTry to run zinstall\e[0m"
+    fi
 }
 
 # Autoload
-# load completion plugins
 for plugin in ${(k)plugins[@]}; do
-  eval "declare -A plugin_data=${plugins[$plugin]}"
-    
-  if [[ ${plugin_data[completion]} -eq 0 ]]; then
-    # echo "$plugin is a completion plugin"
     zload $plugin
-  fi
 done
 
+# compinit after plugins loaded
 autoload -U compinit && compinit -d ~/.cache/zsh/zcompdump-$ZSH_VERSION
-
-for plugin in ${(k)plugins[@]}; do
-  eval "declare -A plugin_data=${plugins[$plugin]}"
-    
-  if [[ ${plugin_data[completion]} -ne 0 ]]; then
-    # echo "$plugin is not a completion plugin"
-    zload $plugin
-  fi
-done
 
 zupdate() {
 {
-  mktemp -d /tmp/.uz_cache > /dev/null
+    mktemp -d /tmp/.uz_cache > /dev/null
+    local -aU to_update=("${UZ_PLUGIN_PATH}"/*(N))
 
-  declare -a to_update
-  local index=1
-  for p in $(command ls -d ${UZ_PLUGIN_PATH}/*); do
-    for plugin in ${(k)plugins[@]}; do
-      eval "declare -A plugin_data=${plugins[$plugin]}"
-      if [[ ${p##*/} == ${plugin##*/} && ${plugin_data[frozen]} -ne 0 ]]; then
-        to_update[$index]=$p
-        ((index++))
-      fi
-    done
-  done
-
-  # echo ${to_update[@]}
-
-  local i=1
-  local j=1
-	for p in ${to_update[@]}; do
+    local i=1
+	for p in $to_update; do
 		file[$i]=$(mktemp /tmp/.uz_cache/uz_cache.XXXX)
 		git -C $p pull &> $file[$i] &
 		((i++))
 	done
 
-  wait
-  echo ""
+    wait
+    echo ""
 
-	for p in ${to_update[@]}; do
+    i=1
+	for p in $to_update; do
 		echo -ne "\e[1;32m${p##*/}: \e[0m\n"
-		command cat $file[$j]
-	echo ""
-	((j++))
-  done
+		command cat $file[$i]
+        echo ""
+        ((i++))
+    done
 
-  rm -rf /tmp/.uz_cache
+    rm -rf /tmp/.uz_cache
 } | grep -Ev "^[\d{0,3}]$" }
 
 zclean() {
-	for p in $(comm -23 <(command ls -1d ${UZ_PLUGIN_PATH}/* | sort) <(printf '%s\n' $UZ_PLUGINS | sort)); do
+    local -aU all_plugins=("${UZ_PLUGIN_PATH}"/*(N))
+	for p in $(comm -23 <(printf '%s\n' $all_plugins | sort) <(printf '%s\n' $UZ_PLUGINS | sort)); do
 		while read "?Are you sure to clean $(echo "$p" | awk -F'/' '{print $NF}')? [y/n] " input; do
 			if [[ $input = "y" || $input = "Y" ]]; then
 				echo -e "\e[1;33mCleaning:\e[0m \e[3m${p}\e[0m"
